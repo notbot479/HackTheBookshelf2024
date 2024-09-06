8a5782b0ab114318b736767ab9a16cf5e16cf76a
Max <avdeenkomaxim123@gmail.com>
2024-09-07 00:59:02 +0500

    feat: add custom jwt token auth

---
+++ backend/jwt_auth/serializers.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class GetTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+        )
+        return user
