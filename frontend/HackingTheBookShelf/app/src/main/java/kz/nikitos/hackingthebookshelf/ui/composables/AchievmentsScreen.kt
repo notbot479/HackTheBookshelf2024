c9c0dc8a399f973d68761189b15c3e71385ad9a9
Yanke Nikita <tolminhrplay@gmail.com>
2024-09-08 14:22:05 +0500

    Fix not showing achievement received date

--- frontend/HackingTheBookShelf/app/src/main/java/kz/nikitos/hackingthebookshelf/ui/composables/AchievmentsScreen.kt
+++ frontend/HackingTheBookShelf/app/src/main/java/kz/nikitos/hackingthebookshelf/ui/composables/AchievmentsScreen.kt
@@ -1,11 +1,13 @@
 package kz.nikitos.hackingthebookshelf.ui.composables
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import kz.nikitos.hackingthebookshelf.data.utils.TimestampToLocalDateTimeConverter
 import kz.nikitos.hackingthebookshelf.domain.models.Achievment
 import java.time.format.DateTimeFormatter
@@ -24,7 +26,7 @@ fun AchievmentCard(achievment: Achievment) {
     val timestampToLocalDateTimeConverter = TimestampToLocalDateTimeConverter()
     val dateFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy: EEEE HH:mm")
     val achievedDate = timestampToLocalDateTimeConverter.convert(achievment.createdAt)
-    OutlinedCard(onClick = { /*TODO*/ }) {
+    OutlinedCard(onClick = { /*TODO*/ }, modifier = Modifier.padding(top = 16.dp)) {
         Text(text = achievment.title)
         Text(text = "Дата получения: ${achievedDate.format(dateFormatter)}")
     }
