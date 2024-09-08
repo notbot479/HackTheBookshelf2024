4b50d28e068883b234fbb9feaf1e83c1066bb4d4
Max <avdeenkomaxim123@gmail.com>
2024-09-08 08:16:09 +0500

    feat - add simple achievement system based on counters

--- backend/reward_system/admin.py
+++ backend/reward_system/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import UserRewardCouter
+from .models import UserRewardCouter, UserAchievement
 
 
 @admin.register(UserRewardCouter)
@@ -8,3 +8,11 @@ class UserRewardCounterAdmin(admin.ModelAdmin):
     list_display = ('user', 'event_counter', 'books_recovered')  
     search_fields = ('user__username',)  
     list_filter = ('event_counter', 'books_recovered')  
+
+
+@admin.register(UserAchievement)
+class UserAchievementAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'created_at')
+    search_fields = ('user__username', 'name')
+    list_filter = ('created_at',)
+    ordering = ('created_at',)
--- backend/reward_system/models.py
+++ backend/reward_system/models.py
@@ -1,6 +1,28 @@
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.db import models
+from typing import Iterator
 
+
+def counter_reward(old, new, attribute: str, *, k: int) -> Iterator:
+    k1 = (getattr(old, attribute) // k) + 1
+    k2 = (getattr(new, attribute) // k) + 1
+    if k1 == k2: return
+    for i in range(k1, k2):
+        stage = i * k
+        if not stage: continue
+        yield stage
+
+
+class UserAchievement(models.Model):
+    class Meta:
+        verbose_name = 'Достижение'
+        verbose_name_plural = 'Достижения'
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 class UserRewardCouter(models.Model):
     class Meta:
@@ -20,3 +42,21 @@ class UserRewardCouter(models.Model):
         verbose_name='Количество возвращенных книг', #pyright: ignore
         default=0, #pyright: ignore
     )
+
+
+def event_counter_reward(old, new, *, k:int=5) -> None:
+    for stage in counter_reward(old=old,new=new,attribute='event_counter',k=k):
+        name = f'The bar has been reached at {stage} visited events'
+        UserAchievement.objects.get_or_create(user=new.user,name=name) #pyright: ignore
+
+def books_counter_reward(old, new, *, k:int=10) -> None:
+    for stage in counter_reward(old=old,new=new,attribute='books_recovered',k=k):
+        name = f'The bar has been reached at {stage} recovered books'
+        UserAchievement.objects.get_or_create(user=new.user,name=name) #pyright: ignore
+
+@receiver(pre_save, sender=UserRewardCouter)
+def compare_old_new_data(sender, instance, **kwargs): #pyright: ignore
+    if not(instance.pk): return
+    old_instance = sender.objects.get(pk=instance.pk)
+    event_counter_reward(old=old_instance, new=instance)
+    books_counter_reward(old=old_instance, new=instance)
